ea5ca96f29e2f569bf37cd8990c25efc5264132b
Vbeni <benivillafana@gmail.com>
2023-07-01 13:52:04 -0400

    template view for countries

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('countries/', views.CountryList.as_view(), name="country_list" )
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,31 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class CountryList(TemplateView):
+    template_name = "country_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["countries"] = countries
+        return context
+
+
+class Country:
+    def __init__(self, name, image, capital, language, currency, population):
+        self.name = name
+        self.image = image
+        self.capital = capital
+        self.language = language
+        self.currency = currency
+        self.population = population
+
+
+countries = [
+    Country("United States", "https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large.png" , "Washington D.C.", "English", "USD", 331449281),
+    Country("United Kingdom","https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png", "London", "English", "GBP", 66650000),
+    Country("France","https://www.countryflags.com/wp-content/uploads/france-flag-png-large.png","Paris", "French", "EUR", 67060000),
+    Country("Japan","https://www.countryflags.com/wp-content/uploads/japan-flag-png-large.png", "Tokyo", "Japanese", "JPY", 126500000),
+    Country("Canada","https://www.countryflags.com/wp-content/uploads/canada-flag-png-large.png", "Ottawa", "English and French", "CAD", 37742154),
+    Country("Australia","https://www.countryflags.com/wp-content/uploads/flag-jpg-xl-9-2048x1024.jpg","Canberra", "English", "AUD", 25600000)
+]
