1ec25dca87dce527cabcf047d540f4981cbf74ac
Vbeni <benivillafana@gmail.com>
2023-07-01 11:55:05 -0400

    about/home template view

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.views import View 
 from django.http import HttpResponse 
+from django.views.generic.base import TemplateView
 
 
 
-
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Travel Home")
+class Home(TemplateView):
+    template_name = "home.html"
     
 
-class About(View):
-    def get(self, request):
-        return HttpResponse("Travel About")
+class About(TemplateView):
+    template_name = "about.html"
