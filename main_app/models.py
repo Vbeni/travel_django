f3e56e7d2948cb0a87c21d119b906096876e1010
Vbeni <benivillafana@gmail.com>
2023-07-05 12:21:01 -0400

    added destination model

--- main_app/models.py
+++ main_app/models.py
@@ -9,9 +9,20 @@ class Country(models.Model):
     currency = models.CharField(max_length=200, default='No Currency')
     population = models.IntegerField(default=0)
 
-    
+
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
+
+class Destination(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="destinations")
+    best_time_to_visit = models.CharField(max_length=200)
+    budget_range = models.CharField(max_length=200)
+    image = models.URLField(max_length=400, default='No Image URL')
+    
+    def __str__(self):
+        return self.title
