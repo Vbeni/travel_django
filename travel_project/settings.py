83e44161c83ca24503c2f608e6be2ba4173d4f62
Vbeni <benivillafana@gmail.com>
2023-07-06 17:18:04 -0400

    more dep.issues

--- travel_project/settings.py
+++ travel_project/settings.py
@@ -12,16 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 environ.Env()
 environ.Env.read_env()
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 
